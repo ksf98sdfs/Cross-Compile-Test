542503c83c8866c8ea0912d7b1d4562f0b126bd7
ksf98sdfs <kasdnoasi@outlook.com>
2024-09-25 23:44:18 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,4 @@
-#include <QApplication>
-#include <QMainWindow>
-#include <QUrl>
-#include <QtNetwork/QNetworkAccessManager>
-#include <QtNetwork/QNetworkRequest>
-#include <QtNetwork/QNetworkReply>
-
 int main(int argc, char **argv)
 {
-  QApplication* app = new QApplication(argc, argv);
-
-  QMainWindow* mainWindow = new QMainWindow(nullptr);
-  mainWindow->show();
-  
-  QNetworkAccessManager* mgr = new QNetworkAccessManager(mainWindow);
-  QUrl url("https://www.svt.se/text-tv/100");
-  QNetworkReply* reply = mgr->get(QNetworkRequest(url));
-  QObject::connect(mgr, &QNetworkAccessManager::finished, [reply]() {
-    if (reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() == 200) {
-      return 0;
-    } else {
-      return 1;
-    }
-  });
-
-  QObject::connect(app, &QApplication::aboutToQuit, mainWindow, &QMainWindow::deleteLater);
-  
-  return app->exec();
+  return 1;
 }
